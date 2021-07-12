e6b9f73b2b463c67df10b35f07264a6dd6f0a5f8
David <davidphang431@gmail.com>
2021-07-12 14:27:41 +0700

    Finish html and css

--- src/weather.js
+++ src/weather.js
@@ -2,11 +2,11 @@ const APP_ID = process.env.APP_ID;
 
 async function getWeather(cityName) {
   const data = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APP_ID}`,
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APP_ID}&units=metric`,
     { mode: "cors" }
   );
   const weatherData = await data.json();
-  console.log(weatherData.weather[0].description);
+  console.log(weatherData);
 }
 
 module.exports = {
